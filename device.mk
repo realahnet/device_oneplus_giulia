00219ad631577c57b75b9348b663de08e5f710ab
madmax7896 <madmax78968@gmail.com>
2025-12-22 13:53:23 +0000

    giulia: Set product shipping API level to 35
    Explicitly build required HIDL packages that are no longer included
    when PRODUCT_SHIPPING_API_LEVEL > 34.
    
    Change-Id: I87ffe21b58c57acb2554c010df0ac73605fd707f

--- device.mk
+++ device.mk
@@ -45,6 +45,11 @@ $(call soong_config_set,qtidisplay,sm8650_pxlw_hw_iris7,true)
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/display_id_4630946652107814787.xml:$(TARGET_COPY_OUT_VENDOR)/etc/displayconfig/display_id_4630946652107814787.xml
 
+# HIDL
+PRODUCT_PACKAGES += \
+    android.hidl.allocator@1.0-service \
+    hwservicemanager
+
 # Fingerprint
 $(call soong_config_set_bool,qtidisplay,oplus_udfps,true)
 
@@ -83,6 +88,9 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/recovery/root/vendor/odm/etc/23867/build.IN.prop:$(TARGET_COPY_OUT_ODM)/etc/23867/build.IN.prop \
     $(LOCAL_PATH)/recovery/root/vendor/odm/etc/23867/build.NA.prop:$(TARGET_COPY_OUT_ODM)/etc/23867/build.NA.prop \
     $(LOCAL_PATH)/recovery/root/vendor/odm/etc/23867/build.default.prop:$(TARGET_COPY_OUT_ODM)/etc/23867/build.default.prop
+
+# Shipping API
+PRODUCT_SHIPPING_API_LEVEL := 35
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
