8d68d4489374d3f179e366cea2c0f1c76ff4cf9e
realahnet <syedmusicrealise@gmail.com>
2026-02-28 23:15:02 +0500

    Revert "giulia: [TEMP]: Remove pixelworks impl"
    This reverts commit 90b491c1d5db2eb9beb3a3b2650533313e69eafe.
    
    * 13R only has sw pixelworks impl, so remove the flag for HW Iris7

--- device.mk
+++ device.mk
@@ -42,6 +42,7 @@ PRODUCT_COPY_FILES += \
 $(call soong_config_set,qtidisplay,bitclk_rate_permissive,true)
 $(call soong_config_set,surfaceflinger,frame_rate_category_high,120)
 $(call soong_config_set,surfaceflinger,frame_rate_category_min,1)
+$(call soong_config_set,qtidisplay,sm8650_pxlw_vendor_namespace,vendor/oneplus/giulia)
 
 # DisplayConfig
 PRODUCT_COPY_FILES += \
