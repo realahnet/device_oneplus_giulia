37d3d4941cf0f81f3db3a31abeadf8aa2be1c1d3
realahnet <syedmusicrealise@gmail.com>
2026-03-06 11:48:03 +0500

    giulia: revert to prebuilt audio
    Some OP12 variants on stock do not use lvacfs, and when used here
    counterintuitively causes mic volume to be too low. Stock hal has some
    behavior which resolves this, so when this behavior is implemented,
    this will be reverted later.

--- device.mk
+++ device.mk
@@ -14,21 +14,10 @@ PRODUCT_PACKAGES += \
     tri-state-key-calibrate
 
 # Audio
-PRODUCT_PACKAGES += \
-    libar-acdb \
-    libar-gpr \
-    libar-gsl \
-    liblx-ar_util \
-    liblx-osal \
-    vendor.qti.hardware.AGMIPC@1.0 \
-    vendor.qti.hardware.AGMIPC@1.0-impl
-
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio/audio_policy_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes.xml \
     $(LOCAL_PATH)/configs/audio/dax-default.xml:$(TARGET_COPY_OUT_VENDOR)/etc/dolby/dax-default.xml \
     $(LOCAL_PATH)/configs/audio/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml
-
-$(call soong_config_set,android_hardware_audio,skip_speaker_layout_channel_mask_field,true)
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2780
